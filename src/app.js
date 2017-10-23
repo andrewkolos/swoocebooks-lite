69c67db46872e0635956708e79fc59585f82c4ad
andrewkolos <andrewrkolos@gmail.com>
2017-10-22 23:48:31 -0400

    migrate to typescript

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const template = <p>hello</p>;
-ReactDOM.render(template, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
-const path = require('path');
-
 module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/public"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
 	},
-	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.js$/,
-			exclude: /node_modules/
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
+	
 	devServer: {
-		contentBase: path.join(__dirname, 'public')
-	}
-}
+		contentBase: __dirname + "/public"
+	},
+};
+
